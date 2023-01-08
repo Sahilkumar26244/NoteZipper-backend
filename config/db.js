cfef6963152de722f87e71bef3f84c28cd3cbc30
Sahil Kumar <kumarsahil8981@gmail.com>
2023-01-08 19:00:42 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv").config()
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect("mongodb+srv://Sahilkumar26244:Sahilkr123@mongodbcluster.nrwwxgv.mongodb.net/?retryWrites=true&w=majority" , {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
+        console.log(`Mongoose Connected : ${conn.connection.host}`)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const cors = require('cors')
+
+
+
+
+const app = express();
+app.use(cors());
+// dotenv.config();
+connectDB();
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send("welcome")
+})
+
+
+app.use('/api/users', userRoutes);
+
+app.use('/api/notes', noteRoutes);
+
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = 5000
+app.listen(PORT,() => {
+    console.log("server is started on 5000")
+})
